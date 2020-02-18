31588c7a78fb97b0768a044f6ebb8dd4b43cbe91
biswajit1305 <biswajit1305@gmail.com>
2020-02-18 22:47:48 +0530

    Project to show table data. Show details on click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'task';
+    searchRows = '';
+    detilsArray = [];
+    rows = [
+        {
+            date: new Date(),
+            list_name: 'Name 1',
+            no_of_enties: 0,
+            details: []
+        },
+        {
+            date: new Date(),
+            list_name: 'Name 2',
+            no_of_enties: 1,
+            details: [{
+                id: 1,
+                description: ' I am here.'
+            }]
+        },
+        {
+            date: new Date(),
+            list_name: 'Name 3',
+            no_of_enties: 4,
+            details: [{
+                id: 1,
+                description: ' I am here.'
+            }, {
+                id: 2,
+                description: ' I am here 2.'
+            }, {
+                id: 3,
+                description: ' I am here 3.'
+            }, {
+                id: 4,
+                description: ' I am here 4.'
+            }]
+        }
+    ];
+
+    view(arr) {
+        this.detilsArray = arr;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {FilterPipe} from './filter.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FilterPipe
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
